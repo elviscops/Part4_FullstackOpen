8e6e679b0b6c96ee15a2e8edc2716c269df24e8a
elviscops <elvis.cops93@gmail.com>
2024-05-16 21:55:58 +0200

    Exercise 4.8

--- BlogList/controllers/blogs.js
+++ BlogList/controllers/blogs.js
@@ -1,20 +1,24 @@
 const blogRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogRouter.get('/api/blogs', (request, response) => {
-    Blog.find({})
-        .then(blogs => {
-            response.json(blogs)
-    })
+blogRouter.get('/api/blogs', async (request,response) => {
+    const blogs = await Blog.find({})
+    response.json(blogs)
 })
 
-blogRouter.post('/api/blogs', (request, response) => {
-    const blog = new Blog(request.body)
+blogRouter.post('/api/blogs', async (request, response, next) => {
+    const body = request.body
 
-    blog.save()
-        .then(result => {
-            response.status(201).json(result)
+    const blog = new Blog({
+            title: body.title,
+            author: body.author,
+            url: body.url,
+            likes: body.likes
     })
+
+    const savedBlog = await blog.save()
+    response.status(201).json(savedBlog)
+
 })
 
 module.exports = blogRouter
---
+++ BlogList/tests/blogAPI.test.js
@@ -0,0 +1,63 @@
+const { test, after ,beforeEach} = require('node:test')
+const assert = require('node:assert')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../App')
+const Blog = require('../models/blog')
+const api = supertest(app)
+
+const initialBlogs = [
+    {
+        "title": "Blog Title",
+        "author": "Blog author",
+        "url": "tvnet.lv",
+        "likes": 3,
+        "id": "663921cd7e6e1445b3c7fbcc"
+    },
+    {
+        "title": "Truth about Michele ",
+        "author": "Barack Obama",
+        "url": "Google.com",
+        "likes": 14,
+        "id": "663921cd723e1445b3c7fbcc"
+      },
+    {
+        "title": "How to make a Blog",
+        "author": "Me",
+        "url": "https://fullstackopen.com/",
+        "likes": 13,
+        "id": "663d03c03ec70ae432ab9fc8"
+    }
+]
+
+beforeEach( async () => {
+    await Blog.deleteMany({})
+
+    let blogObject = new Blog(initialBlogs[0])
+    await blogObject.save()
+  
+    blogObject = new Blog(initialBlogs[1])
+    await blogObject.save()
+
+    blogObject = new Blog(initialBlogs[2])
+    await blogObject.save()
+
+})
+
+test('blogs are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+test('correct number of blog posts', async () => {
+    const response = await api.get('/api/blogs')  
+    assert.strictEqual(response.body.length, 3)
+  })
+  
+
+
+after(async () => {
+    await mongoose.connection.close()
+})
